476c8f9ccf9e86ad8c8cc4efd548bf3f22c59746
Imanuel Ulbricht <me@imanuel.dev>
2022-07-18 22:53:29 +0200

    Added typescript typings

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export declare function html(strings: String[], ...args: (Function | String | Array<any>)[]);
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import isFunction from './node_modules/lodash.isfunction/index.js';
-
+/**
+ * Renders the given template string and parses the given function, string or array directly into the HTML
+ *
+ * @param {String[]} strings The static parts of the template
+ * @param {Function|String|Array} args The dynamic types of the template
+ * @returns {string} The rendered HTML
+ */
 export function html(strings, ...args) {
     let str = strings[0];
     for (let i = 0; i < args.length; i++) {
-        if (isFunction(args[i])) {
+        if (args[i] instanceof Function) {
             str += args[i]();
         } else if (Array.isArray(args[i])) {
             str += args[i].join('');
         } else {
-            str += args[i]
+            str += args[i];
         }
         str += strings[i + 1];
     }
